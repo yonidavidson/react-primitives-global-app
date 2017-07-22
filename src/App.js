29ef411d3d5c6c1d201fa8ca2689f273f0ff3f3d
yonidavidson <yonidavidson@gmail.com>
2017-07-22 19:13:30 +0300

    managed to get create react app running

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends Component {
 	render() {
 		return (
 			<View style={styles.app}>
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
+				<View style={styles.header}>
+					<Image source={logo} style={styles.logo} alt="logo" />
+					<Text style={styles.text}>Welcome to React</Text>
+				</View>
+				<Text style={styles.intro}>
 					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				</Text>
 			</View>
 		);
 	}
@@ -22,22 +22,27 @@ class App extends Component {
 export default App;
 
 const styles = StyleSheet.create({
-	app: {
+	app: {},
+	text: {
+		textAlign: "center",
+		color: "white"
+	},
+	header: {
+		backgroundColor: "#222",
+		height: "150px",
+    padding: "20px",
+    display: 'flex',
+    alignItems: 'center'
+	},
+	logo: {
+		// animation: "App-logo-spin infinite 20s linear",
+		width: 50,
+		height: 50
+	},
+	intro: {
+		fontSize: "large",
 		textAlign: "center"
 	}
-	// logo: {
-	// 	animation: "App-logo-spin infinite 20s linear",
-	// 	height: "80px"
-	// },
-	// header: {
-	// 	"background-color": "#222",
-	// 	height: "150px",
-	// 	padding: "20px",
-	// 	color: 'white'
-	// },
-	// intro:{
-	//   'font-size':'large'
-	// }
 });
 
 // .App {
