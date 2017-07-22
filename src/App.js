234fa8f3a4ced224bf80a0f25461f66c82e501b5
yonidavidson <yonidavidson@gmail.com>
2017-07-22 19:35:56 +0300

    animation works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import { View, Text, Image, StyleSheet } from "react-primitives";
+import { View, Text, Image, StyleSheet, Animated } from "react-primitives";
 import "./App.css";
 
 class App extends Component {
+	componentWillMount() {
+		this._animatedValue = new Animated.Value(0);
+	}
+
+	componentDidMount() {
+		Animated.timing(this._animatedValue, {
+			toValue: 100,
+			duration: 3000
+		}).start();
+	}
+
 	render() {
+		var interpolatedRotateAnimation = this._animatedValue.interpolate({
+        inputRange: [0, 100],
+      outputRange: ['0deg', '360deg']
+    });
 		return (
 			<View style={styles.app}>
 				<View style={styles.header}>
-					<Image source={logo} style={styles.logo} alt="logo" />
+					<Animated.Image source={logo} style={[styles.logo,{transform: [{rotate: interpolatedRotateAnimation}]}]} alt="logo" />
 					<Text style={styles.text}>Welcome to React</Text>
 				</View>
 				<Text style={styles.intro}>
-					To get started, edit <Text style={styles.code}>src/App.js</Text> and save to reload.
+					To get started, edit <Text style={styles.code}>src/App.js</Text> and
+					save to reload.
 				</Text>
 			</View>
 		);
@@ -30,9 +46,9 @@ const styles = StyleSheet.create({
 	header: {
 		backgroundColor: "#222",
 		height: "150px",
-    padding: "20px",
-    display: 'flex',
-    alignItems: 'center'
+		padding: "20px",
+		display: "flex",
+		alignItems: "center"
 	},
 	logo: {
 		// animation: "App-logo-spin infinite 20s linear",
@@ -42,11 +58,11 @@ const styles = StyleSheet.create({
 	intro: {
 		fontSize: "large",
 		textAlign: "center"
-  },
-  code:{
-    color:'blue',
-    fontSize:'small'
-  }
+	},
+	code: {
+		color: "blue",
+		fontSize: "small"
+	}
 });
 
 // .App {
